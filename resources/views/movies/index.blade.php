440e2ac68a573f32d8a9fea58b385c2ce0a64204
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-17 18:36:37 +0200

    stiled page

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,21 +1,24 @@
 @extends('layout.app')
 @section('page-title', 'Movies')
 
+
+
 @section('main-content')
-    <section class="container">
+    <section class="header">
+        <div class="container d-flex justify-content-center">
+            <h1>movie list</h1>
+    </section>
+    <section class="container d-flex justify-content-center">
         @foreach ($movies as $movie)
-            <div class="row">
+            <div>
                 <div class="card">
                     <div class="card-body"></div>
-                        <h1><strong>{{$movie['title']}}</strong></h1>
+                        <h2><strong>{{$movie['title']}}</strong></h2>
                         <h2><strong>Movie's original title: </strong> {{ $movie['original_title']}}</h2>
                         <p><strong>Movie's nationality: </strong> {{$movie['nationality'] }}</p>
                         <p><strong>Movie's date: </strong> {{ $movie['date'] }}</p>
                         <p><strong>Movie's vote: </strong> {{ $movie['vote'] }}</p>
                     </div>
-
-
-
         @endforeach
         </div>
     </section>
